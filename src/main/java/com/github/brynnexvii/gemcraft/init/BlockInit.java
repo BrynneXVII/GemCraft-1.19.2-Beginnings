7d1556b86f4d932ab46373e8e69cf859ffebc39b
BrynneXVII <BrynneXVII@users.noreply.github.com>
2023-01-06 01:49:18 -0500

    Loot tables
    Block item method and loot table packages

--- src/main/java/com/github/brynnexvii/gemcraft/init/BlockInit.java
+++ src/main/java/com/github/brynnexvii/gemcraft/init/BlockInit.java
@@ -1,13 +1,24 @@
 package com.github.brynnexvii.gemcraft.init;
 
 import com.github.brynnexvii.gemcraft.GemCraft;
+import com.google.common.base.Supplier;
 
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class BlockInit {
 
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GemCraft.MODID);
+	
+	//Registry method for block items
+	private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> supplier, Item.Properties properties){
+		RegistryObject<T> block = BLOCKS.register(name, supplier);
+		ItemInit.ITEMS.register(name, () -> new BlockItem(block.get(), properties));
+		return block;
+	}
 	
 }
